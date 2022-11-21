9e672c01404d1c626b68fc5eb3ad1ed7bf5bcdff
rabin niroula <rabinniroula99@gmail.com>
2022-11-21 22:10:04 +0545

    Finished partially. Gotta change the whole code to do EC's API now.

--- kasleJitla.py
+++ kasleJitla.py
@@ -1,15 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+provinces = ['Province-1', 'Madhesh', 'Bagmati', 'Gandaki', 'Lumbini', 'Karnali', 'Sudurparschim']
+print('Which province are you from?')
+for i in range(0, len(provinces)):
+    print(f'{i+1}. {provinces[i]}')
+pradesh = int(input('> ').strip())
+
+district = input('Well, the problem with using open-source software is finding out how lazy the programmers are. I\'m lazy too and printing out a whole list of 77 districts just for you to type a number seemed like a misuse of processing power. Please state the name of your district.\n> ').strip().lower()
+
 rootUrl = 'https://election.ekantipur.com/'
-page = requests.get(f'{rootUrl}/pradesh-1/district-jhapa?lng=eng')
+page = requests.get(f'{rootUrl}/pradesh-{pradesh}/district-{district}?lng=eng')
 soup = BeautifulSoup(page.content, 'html.parser')
 
 rslt2079 = soup.select('.election-2079')
 rsltConsts = rslt2079[0].select('.col-md-6.col-xl-4')
 numOfConsts = len(rsltConsts)
 
-n = int(input(f"Jhapa has {numOfConsts} constituencies. Which would you like to select?\n> ").strip())
+n = int(input(f'{district} has {numOfConsts} constituencies. Which would you like to select?\n> ').strip())
 
 candies = rsltConsts[n-1].find_all('div', 'candidate-wrapper')
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Kasle Jitla | कस्ले जित्ला
+
+A beautiful soup scraper to scrape the **_juicy_** election details from **_ekantipur_**'s website. **FUCK!!!** I just found out that the election commision's website is calling APIs. I just spent the last hour or so reading bs4's documentation. 😤😤 I'm going to redo this whole thing again.
+
+## Usage
+
+```console
+foo@bar:~$ python kasleJitla.py
